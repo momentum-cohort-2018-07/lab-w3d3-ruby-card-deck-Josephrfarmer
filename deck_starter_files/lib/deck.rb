40f74c78412131a5e9657c45ae57d049c4c8f376
josephrfarmer <josephr.farmer24@gmail.com>
2018-08-15 18:06:32 -0400

    first commit
    should have done one sooner...

--- deck_starter_files/lib/deck.rb
+++ deck_starter_files/lib/deck.rb
@@ -1,4 +1,32 @@
 require_relative "card"
 
 class Deck
+
+
+  deck = {
+    clubs:    [:A,2,3,4,5,6,7,8,9,10,:J,:Q,:K],
+    diamonds: [:A,2,3,4,5,6,7,8,9,10,:J,:Q,:K],
+    hearts:   [:A,2,3,4,5,6,7,8,9,10,:J,:Q,:K],
+    spades:   [:A,2,3,4,5,6,7,8,9,10,:J,:Q,:K]
+  }
+
+  def initialize
+    @deck = []
+    ranks = [:A,2,3,4,5,6,7,8,9,10,:J,:Q,:K]
+    ranks.each do 
+      card.new
+    
+  end 
+
+  def draw
+
+  end
+  
+  def shuffle
+
+  end
+
+  def cards_left
+
+  end
 end
